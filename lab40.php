8fd24eb7bed38c268434acb2efc2bf34e13ef51d
leo <yuting870116@gmail.com>
2021-12-20 15:28:25 +0800

    201220_1530

--- lab40.php
+++ lab40.php
@@ -1,9 +1,24 @@
+<table border="1" width = "100%">
 <?php
-    $fp = fopen('./dir1/file2','r');
+    $fp = fopen('./dir1/maskdata.csv','r');
+    $header = fgets($fp);
+    $head = explode(',',$header); //字串切割explode('用什麼切',elm)
+    echo '<tr>';
+    echo "<th>{$head[1]}</th>";
+    echo "<th>{$head[2]}</th>";
+    echo "<th>{$head[4]}</th>";
+    echo "<th>{$head[5]}</th>";
 
     while($line = fgets($fp)){
-        echo $line .'<br>';
+        $data = explode(',',$line);
+        echo '<tr>';
+        echo "<td>{$data[1]}</td>";
+        echo "<td>{$data[2]}</td>";
+        echo "<td>{$data[4]}</td>";
+        echo "<td>{$data[5]}</td>";
+        echo '</tr>';
     } ;
     fclose($fp);
 
 ?>
+</table>
